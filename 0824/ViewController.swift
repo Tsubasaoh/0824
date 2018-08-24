f8f02901e9aef40f89a05a0a3b253169b7cc2ec6
“Zorro” <ekwzorrowing@gmail.com>
2018-08-24 17:33:55 +0800

    Initial Commit

---
+++ 0824/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0824
+//
+//  Created by 王奕功 on 2018/8/24.
+//  Copyright © 2018年 王奕功. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
