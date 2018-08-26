894a8ee2c05b5090197ecc5f24142994d7833fa4
“Zorro” <ekwzorrowing@gmail.com>
2018-08-26 12:06:12 +0800

    luigi and star

--- 0824/ViewController.swift
+++ 0824/ViewController.swift
@@ -7,9 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+//一段outlet與action的故事
+    @IBOutlet weak var luigiImageView: UIImageView!
+    @IBOutlet weak var starImageView: UIImageView!
+    @IBOutlet weak var meetTextfield: UITextField!
+//拉動slider讓圖案往中間靠近
+    @IBAction func closetoMeSlider(_ sender: UISlider) {
+        luigiImageView.frame.origin.x = CGFloat(156+124*sender.value)
+        starImageView.frame.origin.x = CGFloat(508-124*sender.value)
+//距離越近速度越快
+    if sender.value == Float(0.6) {
+        luigiImageView.frame.origin.x = CGFloat(156+30*sender.value)
+        starImageView.frame.origin.x = CGFloat(508-30*sender.value)
+    }else{
+        luigiImageView.frame.origin.x = CGFloat(156+124*sender.value)
+        starImageView.frame.origin.x = CGFloat(508-124*sender.value)
+//愛要說粗乃
+        var speechUtterance = AVSpeechUtterance()
+        let synthesizer = AVSpeechSynthesizer()
+        if sender.value >= 0.9 {
+            speechUtterance = AVSpeechUtterance(string: "Wait for me,dear")
+            speechUtterance.pitchMultiplier = 0.3
+            synthesizer.speak(speechUtterance)
+//君子愛財取之有道？
+        if sender.value == 1.0 {
+            meetTextfield.text = "Finally~♥️"
+        } else {
+            meetTextfield.text = ""
+        }
+        
+    }
+    }
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,4 +50,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
